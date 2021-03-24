de26cdd7f4f340a4a53bcff71d96329dd6d77cb6
elies127 <elies1324@gmail.com>
2021-03-24 10:56:20 +0100

    checkpoint 2

--- examen.py
+++ examen.py
@@ -1,4 +1,5 @@
-
+from libro import Libro
+from autor import Autor
 def get_list(fichero):
     
     f = open(fichero, mode="rt", encoding="utf-8")
@@ -7,7 +8,10 @@ def get_list(fichero):
         string = string + linea
 
     f.close()
-    return generar_diccionario(string)
+    if string == "":
+        raise ValueError("El archivo está vacio")
+    else:
+        return generar_diccionario(string)
 def generar_diccionario(string):
     string=string.lower().split()
     dic={}
@@ -15,6 +19,18 @@ def generar_diccionario(string):
         if len(word) not in dic:
             dic[len(word)] = set()
         dic[len(word)].add(word)
-    return dic    
+    return dic
+
+
+
+# --------------------MAIN---------------------
 print(get_list("fichero.txt"))
-print(get_list("vacio.txt"))
+try:
+    print(get_list("vacio.txt"))
+except ValueError:
+    print("ValueError: No se ha detectado ninguna palabra en el fichero")
+
+l1 = Libro(autor = Autor(id_autor = 1, nombre = "Miguel de Cervantes", apellido = "Saavedra"), titulo = "Don Quijote", anyo = 1522)
+l2 = Libro(autor = Autor(id_autor = 2, nombre = "Miguel", apellido = "Jose"), titulo = "Doña Quijota", anyo = 1901)
+l3 = Libro(autor = Autor(id_autor = 3, nombre = "Miguelito", apellido = "Jose Paco"), titulo = "Don Moderno 2040", anyo = 2040)
+l4 = Libro(autor = Autor(id_autor = 4, nombre = "Alejandro", apellido = "Maria"), titulo = "Harry Potter 2020", anyo = 2020) 
---
+++ libro.py
@@ -0,0 +1,6 @@
+from autor import Autor
+class Libro:
+    def __init__(self, autor, titulo, anyo):
+        self.__autor = autor
+        self.__titulo = titulo
+        self.__anyo = anyo
